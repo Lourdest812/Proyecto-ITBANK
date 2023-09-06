972b427167ef82fcb09d066c15761e5a61b196ed
Lourdes Toledo <lourdest812@gmail.com>
2023-09-06 17:01:08 -0300

    Arreglos de archivos css

---
+++ itbank/src/body/body.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "../login/Login";
+
+function Body(){
+    return(
+        <Login/>
+    );
+}
--- itbank/src/login/Login.js
+++ itbank/src/login/Login.js
@@ -2,15 +2,23 @@ import React from 'react'
 import Logo from '../assets/img/bxs-bank.svg'
 import Form from './Form';
 import '../css/login.css'
+import '../css/mainPage.css'
 import LoginPicture from './LoginPicture';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Login(){
   return (
+    <>
+    <Header/>
           <div className="container">
-          <h1>Inicio de sesión</h1>
-          <LoginPicture/>
-          <Form />
-          </div>
+            
+            <h1>Inicio de sesión</h1>
+            <LoginPicture/>
+            <Form />
+            </div>
+    <Footer/>
+    </>
   );
 }
 

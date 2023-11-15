ae46025522d88e5f84c6a672c0bd76481c976511
Lourdes Toledo <lourdest812@gmail.com>
2023-11-15 01:39:39 -0300

    Actualización en modelos login y movimientos

--- Backend/Sprint 7/homebanking/login/models.py
+++ Backend/Sprint 7/homebanking/login/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PerfilUsuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
--- Backend/Sprint 7/homebanking/movimientos/models.py
+++ Backend/Sprint 7/homebanking/movimientos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from cuentas.models import Cuenta
 
-# Create your models here.
+class Movimiento(models.Model):
+    cuenta = models.ForeignKey(Cuenta, on_delete=models.CASCADE)
+    monto = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Movimiento de {self.cuenta}"
